843fa42fb4b79b9e69b2e931cbbaf628c447993c
Kok Hoe Lim <43999043+hmlimkokhoe@users.noreply.github.com>
2021-12-08 17:35:58 +0100

    Sandbox (#1)
    * more comments and run instructions
    
    * move object functions to action creator fetchUsers
    
    * readability changes
    
    Co-authored-by: kok-hoe.lim <kok-hoe.lim@ippen-digital.de>

---
+++ readme.md
@@ -0,0 +1,4 @@
+# How to run redux thunk middleware demo:
+1. Go to directory: `cd redux-demo`
+1. Install axios and redux libraries: `npm install`
+2. Fetch JSON example using action creator: `node ./asyncActions.js`
--- redux-demo/asyncActions.js
+++ redux-demo/asyncActions.js
@@ -1,8 +1,16 @@
+/**
+ * Two important packages:
+ * Axios - for API endpoint requests
+ * redux - for Redux API
+ * redux-thunk - define async action creators. thunk is middleware and allows us to write asynchronous logic or side effects in general
+ */
 const redux = require('redux')
 const thunkMiddleware = require('redux-thunk').default
 const axios = require('axios')
-const createStore = redux.createStore
-const applyMiddleware = redux.applyMiddleware
+
+const FETCH_USERS_REQUEST = 'FETCH_USERS_REQUEST';
+const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS';
+const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE';
 
 const initialState = {
   loading: false,
@@ -10,53 +18,23 @@ const initialState = {
   error: ''
 }
 
-const FETCH_USERS_REQUEST = 'FETCH_USERS_REQUEST'
-const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS'
-const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE'
-
-const fetchUsersRequest = () => {
-  return {
-    type: FETCH_USERS_REQUEST
-  }
-}
-
-const fetchUsersSuccess = users => {
-  return {
-    type: FETCH_USERS_SUCCESS,
-    payload: users
-  }
-}
-
-const fetchUsersFailure = error => {
-  return {
-    type: FETCH_USERS_FAILURE,
-    payload: error
-  }
-}
-
-const fetchUsers = () => {
-  return function (dispatch) {
-    dispatch(fetchUsersRequest())
-    axios
-      .get('https://jsonplaceholder.typicode.com/users')
-      .then(response => {
-        // response.data is the users
-        const users = response.data.map(user => user.id)
-        dispatch(fetchUsersSuccess(users))
-      })
-      .catch(error => {
-        // error.message is the error message
-        dispatch(fetchUsersFailure(error.message))
-      })
-  }
-}
-
+/**
+ * Initialize state and define actions. Required for createStore function
+ * @param {*} state 
+ * @param {*} action 
+ * @returns 
+ */
 const reducer = (state = initialState, action) => {
   console.log(action.type)
+  console.log('====')
+
+  /**
+   * Match Action payload and return corresponding object
+   */
   switch (action.type) {
     case FETCH_USERS_REQUEST:
       return {
-        ...state,
+        ...state, //copy existing state with spread operator
         loading: true
       }
     case FETCH_USERS_SUCCESS:
@@ -71,9 +49,58 @@ const reducer = (state = initialState, action) => {
         users: [],
         error: action.payload
       }
+    default:
+      return {
+        error: action.payload
+      };
   }
 }
 
-const store = createStore(reducer, applyMiddleware(thunkMiddleware))
-store.subscribe(() => { console.log(store.getState()) })
-store.dispatch(fetchUsers())
+/**
+ * Action creator, so it returns a action. This is dispatched to the store.
+ * @returns non-pure function, so it can have side effects like async API calls. it can also dispatch actions thanks to dispatch param
+ */
+ const fetchUsers = () => {
+
+  const fetchUsersRequest = () => {
+    return {
+      type: FETCH_USERS_REQUEST
+    }
+  }
+  
+  const fetchUsersSuccess = users => {
+    return {
+      type: FETCH_USERS_SUCCESS,
+      payload: users
+    }
+  }
+  
+  const fetchUsersFailure = error => {
+    return {
+      type: FETCH_USERS_FAILURE,
+      payload: error
+    }
+  }
+
+  return function (dispatch) {
+    dispatch(fetchUsersRequest()) //set loading to true
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        // response.data is the users
+        const users = response.data.map(user => user.id) //for each user return user.id
+        dispatch(fetchUsersSuccess(users)) //store user in state
+      })
+      .catch(error => {
+        dispatch(fetchUsersFailure(error.message))
+      })
+  }
+}
+
+const createStore = redux.createStore
+const applyMiddleware = redux.applyMiddleware
+const store = createStore(reducer, applyMiddleware(thunkMiddleware)) //pass middleware to createStore function, 
+                                                                     //allows fetchUsers() to return function instead of action object
+
+store.subscribe(() => { console.log(store.getState()) }) //subscribe is state listener
+store.dispatch(fetchUsers()) //dispatch async action creator
