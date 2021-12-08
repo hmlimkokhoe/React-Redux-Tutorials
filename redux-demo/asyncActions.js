37dd836b7b1840b71688c08ac3289db7906cb6ae
kok-hoe.lim <kok-hoe.lim@ippen-digital.de>
2021-12-08 17:23:34 +0100

    readability changes

--- redux-demo/asyncActions.js
+++ redux-demo/asyncActions.js
@@ -1,15 +1,13 @@
 /**
  * Two important packages:
  * Axios - for API endpoint requests
- * redux-thunk - define async action creators, works as middleware for redux store
+ * redux - for Redux API
+ * redux-thunk - define async action creators. thunk is middleware and allows us to write asynchronous logic or side effects in general
  */
 const redux = require('redux')
 const thunkMiddleware = require('redux-thunk').default
 const axios = require('axios')
 
-/**
- * STATE & STATIC VAR DEFINITION
- */
 const FETCH_USERS_REQUEST = 'FETCH_USERS_REQUEST';
 const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS';
 const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE';
@@ -21,10 +19,48 @@ const initialState = {
 }
 
 /**
+ * Initialize state and define actions. Required for createStore function
+ * @param {*} state 
+ * @param {*} action 
+ * @returns 
+ */
+const reducer = (state = initialState, action) => {
+  console.log(action.type)
+  console.log('====')
+
+  /**
+   * Match Action payload and return corresponding object
+   */
+  switch (action.type) {
+    case FETCH_USERS_REQUEST:
+      return {
+        ...state, //copy existing state with spread operator
+        loading: true
+      }
+    case FETCH_USERS_SUCCESS:
+      return {
+        loading: false,
+        users: action.payload,
+        error: ''
+      }
+    case FETCH_USERS_FAILURE:
+      return {
+        loading: false,
+        users: [],
+        error: action.payload
+      }
+    default:
+      return {
+        error: action.payload
+      };
+  }
+}
+
+/**
  * Action creator, so it returns a action. This is dispatched to the store.
  * @returns non-pure function, so it can have side effects like async API calls. it can also dispatch actions thanks to dispatch param
  */
-const fetchUsers = () => {
+ const fetchUsers = () => {
 
   const fetchUsersRequest = () => {
     return {
@@ -61,47 +97,10 @@ const fetchUsers = () => {
   }
 }
 
-/**
- * Initialize state and define actions. Required for createStore function
- * @param {*} state 
- * @param {*} action 
- * @returns 
- */
-const reducer = (state = initialState, action) => {
-  console.log(action.type)
-  console.log('====')
-
-  /**
-   * MATCH ACTION FROM API CALL
-   */
-  switch (action.type) {
-    case FETCH_USERS_REQUEST:
-      return {
-        ...state, //copy existing state with spread operator
-        loading: true
-      }
-    case FETCH_USERS_SUCCESS:
-      return {
-        loading: false,
-        users: action.payload,
-        error: ''
-      }
-    case FETCH_USERS_FAILURE:
-      return {
-        loading: false,
-        users: [],
-        error: action.payload
-      }
-    default:
-      return {
-        error: action.payload
-      };
-  }
-}
-
 const createStore = redux.createStore
 const applyMiddleware = redux.applyMiddleware
-const store = createStore(reducer, applyMiddleware(thunkMiddleware)) //pass middleware to createStore function, allows method to return function instead of action object
+const store = createStore(reducer, applyMiddleware(thunkMiddleware)) //pass middleware to createStore function, 
+                                                                     //allows fetchUsers() to return function instead of action object
 
 store.subscribe(() => { console.log(store.getState()) }) //subscribe is state listener
 store.dispatch(fetchUsers()) //dispatch async action creator
